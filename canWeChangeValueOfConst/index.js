7eacbb7326f7e6a5aa59dfedb3e0de2b8f3d89d1
Virendra patel <patelvirendra62@gmail.com>
2023-01-05 11:22:36 +0530

    canWeChangeValueOfConstVariable

---
+++ canWeChangeValueOfConst/index.js
@@ -0,0 +1,18 @@
+// const student = "Virendra";
+
+// student = "Harsh";
+
+const student = {
+  name: "virendra",
+};
+console.log(student);
+
+student.name = "Harsh";
+
+const marks = [];
+
+marks.push(23);
+
+marks = [];
+
+console.log(student);
